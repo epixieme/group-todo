8d34caae6d638d58daaf0acbe323422f43dd22c8
epixieme <kirstie_hayes@hotmail.com>
2022-09-06 10:27:24 +0100

    Added todo text edit and update features

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 const deleteBtn = document.querySelectorAll(".del");
 const todoItem = document.querySelectorAll("input.not");
 const todoComplete = document.querySelectorAll("input.filter-green");
+const editBtn = document.querySelectorAll(".edit");
+const updateBtn = document.querySelectorAll(".update");
+
+
 
 Array.from(deleteBtn).forEach((el) => {
   el.addEventListener("click", deleteTodo);
@@ -13,6 +17,22 @@ Array.from(todoItem).forEach((el) => {
 Array.from(todoComplete).forEach((el) => {
   el.addEventListener("click", markIncomplete);
 });
+
+// edit the todo item
+
+if (editBtn) {
+  const editTodo = Array.from(editBtn).forEach((element) => {
+    element.addEventListener("click", editTodoInfo);
+  });
+}
+
+//   update the todo item
+if (updateBtn) {
+  const updateTodo = Array.from(updateBtn).forEach((element) => {
+    element.addEventListener("click", updateTodoInfo);
+  });
+}
+
 
 async function deleteTodo() {
   const todoId = this.parentNode.dataset.id;
@@ -67,3 +87,38 @@ async function markIncomplete() {
     console.log(err);
   }
 }
+
+
+function editTodoInfo() {
+  let id = this.parentNode.dataset.id;
+  let todoText = this.parentNode.children[1];
+  if (id) {
+    todoText.setAttribute("contenteditable", true);
+    todoText.style.background = "#C8CFD2";
+    todoText.border = "1px solid grey";
+  }
+}
+
+ async function updateTodoInfo() {
+  let todoId = this.parentNode.dataset.id;
+  let todoText = this.parentNode.children[1];
+console.log(todoText.innerText)
+
+   if (todoId) {
+    todoText.setAttribute("contenteditable", false);
+    todoText.style.background = "none";
+    todoText.border = "none";
+  }
+    try {
+      await fetch("/todos/updateTodo", {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          todoIdFromJSFile: todoId,
+          textInput:todoText.innerText
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
